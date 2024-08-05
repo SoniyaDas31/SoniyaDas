1a4d994015383dfc0e97974de82a5f1810d36013
SoniyaDas31 <dassubbulakshmi@gmail.com>
2024-08-05 18:47:48 +0530

    Added Lesson 3

---
+++ lesson3/ArraysObjectsLoops/script.js
@@ -0,0 +1,33 @@
+
+var arrays=[1, 45, 34, 56, 78];
+// console.log(array_numbers);
+
+// for loop for array
+for (let i= 0; i<(arrays.length); i++) {
+    
+    // console.log(array_numbers[i]);
+//console.log(arrays[i]);
+
+}
+
+// for in loop for object
+var obj = {fname:'Soniya Das', age:30, place:'Mumbai'};
+
+for (const i in obj){
+  //  console.log(i);
+}
+
+// While Loop
+let i=0;
+while(i<10){
+    // console.log(i+1);
+    i++;
+}
+
+// do while loop
+let r=0;
+do{
+    console.log(r);
+    r++;
+
+}while(r<=10);
